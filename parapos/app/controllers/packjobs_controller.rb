8989c5e51551a0aa7dbe57e6082a5a3d8a872e67
Paul Gamble <pgamble@gmail.com>
2018-02-10 18:02:36 -0500

    D3: got the edit to work; was skipping over a section when I was reading

--- parapos/app/controllers/packjobs_controller.rb
+++ parapos/app/controllers/packjobs_controller.rb
@@ -11,18 +11,33 @@ class PackjobsController < ApplicationController
     @packjob = Packjob.new
   end
 
+  def edit
+    @packjob = Packjob.find(params[:id])
+  end
+
   def create
-    @packjob = Packjob.new(packjob_parms)
+    @packjob = Packjob.new(packjob_params)
 
     if @packjob.save
       redirect_to @packjob
     else
       render 'new'
-    end    
+    end
+  end
+
+
+  def update
+    @packjob = Packjob.find(params[:id])
+
+    if @packjob.update(packjob_params)
+      redirect_to @packjob
+    else
+      render 'edit'
+    end
   end
 
   private
-    def packjob_parms
+    def packjob_params
       params.require(:packjob).permit(:packer, :rig)
     end
 
